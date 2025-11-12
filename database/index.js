2f4f08b950c37ddb65d5edae53af9ff7afce4f3b
bruninrko123 <balves@byupathway.edu>
2025-11-11 21:42:15 -0300

    ksdj

--- database/index.js
+++ database/index.js
@@ -35,6 +35,9 @@ if (process.env.NODE_ENV == "development") {
 } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: true,
+    },
   });
   module.exports = pool;
 }
